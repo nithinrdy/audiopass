83d888d5bc565e6e4566cd0ab4d32e83dd273b28
nithinrdy <78612244+nithinrdy@users.noreply.github.com>
2026-09-21 00:57:50 +0530

    single instance guard

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@ mod ui;
 mod utils;
 
 fn main() -> eframe::Result<()> {
+    let instance = single_instance::SingleInstance::new(backend::constants::AUDIOPASS_APP_ID).map_err(|error| eframe::Error::AppCreation(Box::new(error)))?;
+    if !instance.is_single() {
+        return Ok(());
+    }
+
     ui::run_app()
 }
